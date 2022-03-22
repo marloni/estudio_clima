8eae671d049f8ec205d5b718767a9439d1d81044
marloni <marlonlasur@gmail.com>
2022-03-22 22:16:41 +0100

    import export

--- core/settings.py
+++ core/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "import_export",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
