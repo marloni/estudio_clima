b3ab922cb0c0df00c9e21da81b2c9c2bf7ef3e1d
marloni <marlonlasur@gmail.com>
2022-03-25 07:48:46 +0100

    Update views.py

--- apps/informes/views.py
+++ apps/informes/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from . import simpleexample
 
 
 class InformesView(TemplateView):
     template_name = "informes.html"
-    context_object_name = "informes"
+    config = {"static_folder": "static", "routes_pathname_prefix": "/informes/"}
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {})
+        return render(request, self.template_name)
 
     def post(self, request, *args, **kwargs):
-        return redirect("/")
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
+        if request.POST.get("action") == "simpleexample":
+            return redirect(simpleexample.app.server)
+        return redirect(self.config["routes_pathname_prefix"])
