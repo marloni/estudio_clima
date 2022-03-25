06a9fd2110d3a79ed8018badfedce667ac0ccfd3
marloni <marlonlasur@gmail.com>
2022-03-25 07:48:44 +0100

    Update urls.py

--- apps/informes/urls.py
+++ apps/informes/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 from apps.informes.views import InformesView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path("", InformesView.as_view(), name="informes"),
-    path("django_plotly_dash/", include("django_plotly_dash.urls")),
 ]
